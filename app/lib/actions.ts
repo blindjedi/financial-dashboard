aba9884ae80d4563730c50ba5da8ada23dbb2691
blindjedi <matt6js@gmail.com>
2024-06-21 20:35:29 -0600

    create server action

---
+++ app/lib/actions.ts
@@ -0,0 +1,52 @@
+'use server';
+
+import { connectClient } from '@/app/lib/data';
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    // const client = new Client({
+    //     connectionString: process.env.POSTGRES_URL,
+    //     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    // });
+
+    // await client.connect();
+    const client = await connectClient();
+
+    try {
+        await client.query(
+            'INSERT INTO invoices (customer_id, amount, status, date) VALUES ($1, $2, $3, $4)',
+            [customerId, amountInCents, status, date]
+        );
+    } finally {
+        await client.end();
+    }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+
+
+}
